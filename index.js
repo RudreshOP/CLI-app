0e62efa3ce338319a251fa1382de2cd687ff18d7
Rudresh Paradkar <61784635+RudreshOP@users.noreply.github.com>
2021-07-23 14:30:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+var highScores = [
+  {
+    name: "Rutvik",
+    score: 3,
+  },
+
+  {
+    name: "Rudresh",
+    score: 2,
+  },
+]
+
+
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Mumbai"
+}, {
+  question: "My favorite superhero would be? ",
+  answer: "Iron Man"
+},
+{
+  question: "My Favourite Cricketer? ",
+  answer: "Rohit Sharma"
+}];
+
+function welcome() {
+ var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome "+ userName + " to DO YOU KNOW Rudresh?");
+}
+
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { 
+    console.log("right!");
+    score = score + 1;
+    
+  } else {
+    console.log("wrong!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
